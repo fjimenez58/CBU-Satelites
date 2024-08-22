603e76a846465546446a15d4c984dc2b2d5e6b1b
fjimenez58 <120272174+fjimenez58@users.noreply.github.com>
2024-08-22 15:02:46 -0500

    Update CodigoPulsador.ino

--- CodigoPulsado.ino
+++ CodigoPulsado.ino
@@ -1,6 +1,3 @@
-// Declaramos una variable llamada LED_BUILTIN y le asignamos el pin número 2
-// Esta variable indica que vamos a usar el pin 2 del Arduino para controlar un LED
-int LED_BUILTIN = 2;
 // La función setup() se ejecuta una vez, al inicio, cuando se enciende o reinicia la placa ESP32.
 // Aquí es donde configuramos los pines (conexiones) de la placa.
 void setup() {
@@ -16,17 +13,17 @@ void setup() {
 void loop() {
   // Declaramos una variable llamada Push_button_state para almacenar el estado del botón.
   // Luego, leemos el valor del pin 0 (donde está conectado el botón) y lo guardamos en Push_button_state.
-  // Si el botón está presionado, el valor será HIGH (alto, 1).
-  // Si el botón no está presionado, el valor será LOW (bajo, 0).
+  // Si el botón está presionado, el valor será LOW.
+  // Si el botón no está presionado, el valor será HIGH.
   int Push_button_state = digitalRead(0);
-  // Ahora, comprobamos si el botón está presionado (si el valor leído es HIGH).
+  // Ahora, comprobamos si el botón está presionado.
   if (Push_button_state == HIGH) {
-    // Si el botón está presionado, apagamos el LED.
-    // Lo hacemos enviando un valor LOW (bajo, 0) al pin 2, que controla el LED.
+    // Si el botón está presionado, encendemos el LED.
+    // Lo hacemos enviando un valor LOW (alto, 0) al pin 2.
     digitalWrite(2, LOW);
   } else {
-    // Si el botón no está presionado, encendemos el LED.
-    // Lo hacemos enviando un valor HIGH (alto, 1) al pin 2.
+    // Si el botón no está presionado, apagamos el LED.
+    // Lo hacemos enviando un valor HIGH (bajo, 1) al pin 2, que controla el LED.
     digitalWrite(2, HIGH);
   }
 }
